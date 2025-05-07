287ae3f674ad7f384e12afbf10e4238fa217d32d
Alok-Kumar2005 <ay747283@gmail.com>
2025-05-07 23:38:23 +0530

    template created

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "DS-Intern-Assignment"
+AUTHOR_USER_NAME = "Alok-Kumar2005"
+SRC_REPO = "ml_project"
+AUTHOR_EMAIL = "ay747283@gmail.com"
+
+setup(
+    name="imago_assignment",
+    version="0.1.0",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
